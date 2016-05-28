fd7e6ba999c34b79c8a02571357340053bdb5816
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-27 21:35:13 -0500

    :rocket: Add button to display simple alert

--- IntroIosNavigation/ViewController.swift
+++ IntroIosNavigation/ViewController.swift
@@ -30,6 +30,18 @@ class ViewController: UIViewController {
     let nextController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
     self.presentViewController(nextController, animated: true, completion: nil)
   }
+  
+  @IBAction func displayAlert() {
+    let alertController = UIAlertController()
+    alertController.title = "Test alert"
+    alertController.message = "This is an example on how to create alerts"
+    
+    let alertAction = UIAlertAction(title: "ok", style:.Default){action in
+      self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    alertController.addAction(alertAction)
+    self.presentViewController(alertController, animated: true, completion: nil)
+  }
 
 }
 
